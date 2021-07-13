82e6b20f341c940f6f36412c93ee66d57285d78e
joaopedrocoelho <joaopedrocoelho2@gmail.com>
2021-07-13 16:24:42 +0800

    added alert on window  close

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+window.addEventListener("beforeunload", function (e) {
+  // Cancel the event
+  e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+  // Chrome requires returnValue to be set
+  e.returnValue = "";
+});
+
 // gets a random integer
 function getRandomInt(min, max) {
   min = Math.ceil(min);
